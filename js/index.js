fe4a21b494b1b4a1724ded548ea119b5539f90f4
wudixiaochongzi <1150964664@qq.com>
2016-05-02 15:38:20 +0800

    完成布局

--- js/index.js
+++ js/index.js
@@ -2,16 +2,26 @@
  * Created by wc on 2016/5/1.
  */
 window.onload=function(){
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
+    var bannerSwiper = new Swiper('.banner .swiper-container', {
+        pagination: '.banner .swiper-pagination',
+        nextButton: '.banner .swiper-button-next',
+        prevButton: '.banner .swiper-button-prev',
         slidesPerView: 1,
         paginationClickable: true,
         spaceBetween: 0,
         centeredSlides: true,
         autoplay: 3000,
+        speed: 600,
         autoplayDisableOnInteraction: false,
         loop: true
     });
+
+    var buySwiper = new Swiper('.buy-content .swiper-container', {
+        pagination: '.buy-content .swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.buy-content .swiper-button-next',
+        prevButton: '.buy-content .swiper-button-prev',
+        spaceBetween: 30
+    });
+
 };
